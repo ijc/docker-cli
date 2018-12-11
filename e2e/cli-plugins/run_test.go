db035c527ef2275d6df53600f567624336ab0610
Ian Campbell <ijc@docker.com>
2018-12-11 14:15:04 +0000

    Add some simple e2e tests for executing CLI plugins
    To help with this add a bad plugin which produces invalid metadata.
    
    Signed-off-by: Ian Campbell <ijc@docker.com>

---
+++ e2e/cli-plugins/run_test.go
@@ -0,0 +1,31 @@
+package cliplugins
+
+import (
+	"testing"
+
+	"gotest.tools/icmd"
+)
+
+func TestRunNonexisting(t *testing.T) {
+	res := icmd.RunCmd(icmd.Command("docker", "nonexistent"))
+	res.Assert(t, icmd.Expected{
+		ExitCode: 1,
+		Err:      "docker: 'nonexistent' is not a docker command.\nSee 'docker --help'",
+	})
+}
+
+func TestRunBad(t *testing.T) {
+	res := icmd.RunCmd(icmd.Command("docker", "badmeta"))
+	res.Assert(t, icmd.Expected{
+		ExitCode: 1,
+		Err:      "docker: 'badmeta' is not a docker command.\nSee 'docker --help'",
+	})
+}
+
+func TestRunGood(t *testing.T) {
+	res := icmd.RunCmd(icmd.Command("docker", "helloworld"))
+	res.Assert(t, icmd.Expected{
+		ExitCode: 0,
+		Out:      "Hello World!",
+	})
+}

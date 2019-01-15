jeanlaurent/cli

Add basic framework for writing a CLI plugin
That is, the helper to be used from the plugin's `main`.

Also add a `helloworld` plugin example and build integration.

Signed-off-by: Ian Campbell <ijc@docker.com>

cli-plugins/metadata.go
@@ -1,0 +1,19 @@
+package cliplugins
+
+var (
+	// NamePrefix is the prefix required on all plugin binary names
+	NamePrefix = "docker-"
+
+	// MetadataSubcommandName is the name of the plugin subcommand
+	// which must be supported by every plugin and returns the
+	// plugin metadata.
+	MetadataSubcommandName = "docker-cli-plugin-metadata"
+)
+
+// Metadata provided by the plugin
+type Metadata struct {
+	Version          string
+	Vendor           string
+	ShortDescription string
+	SchemaVersion    string
+}

ijc/docker-cli

cli-plugins/manager/metadata.go
@@ -1,0 +1,19 @@
+package manager
+
+const (
+	// NamePrefix is the prefix required on all plugin binary names
+	NamePrefix = "docker-"
+
+	// MetadataSubcommandName is the name of the plugin subcommand
+	// which must be supported by every plugin and returns the
+	// plugin metadata.
+	MetadataSubcommandName = "docker-cli-plugin-metadata"
+)
+
+// Metadata provided by the plugin
+type Metadata struct {
+	Version          string
+	Vendor           string
+	ShortDescription string
+	SchemaVersion    string
+}